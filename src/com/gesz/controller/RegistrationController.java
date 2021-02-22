cb5e0c4a4e7d7b79ebd450a854d786a7974d6d29
PersonalAccessToken <ralphc027@gmail.com>
2021-02-22 16:58:49 +0800

    committing WebServlet to UserRegistrationv3-Branch

--- src/com/gesz/controller/RegistrationController.java
+++ src/com/gesz/controller/RegistrationController.java
@@ -1,5 +1,40 @@
 package com.gesz.controller;
 
-public class RegistrationController {
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import com.gesz.model.User;
+import com.gesz.service.Authenticator;
+
+@WebServlet("/pages/register")
+public class RegistrationController extends HttpServlet{
+private static final long serialVersionUID = -3435554487273689111L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		RequestDispatcher dispatcher = null;
+		
+		Authenticator authenticator = new Authenticator();
+		String result = authenticator.authenticate(username,password);
+		if(result.equals("success")) {
+			dispatcher = request.getRequestDispatcher("pages/adminControl.jsp");
+			User user = new User(username,password);
+			request.setAttribute("user", user);
+			HttpSession session = request.getSession();
+			session.setAttribute("user", user);
+		}else {
+			request.setAttribute("loginMessage", "Login failed, please try again");
+			dispatcher=request.getRequestDispatcher("pages/adminLogin.jsp");
+		}
+		dispatcher.forward(request, response);
+	}
 
 }
