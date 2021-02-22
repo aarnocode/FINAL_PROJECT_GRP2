3af9325fff62cc926148254a030b6d796fefb838
PersonalAccessToken <ralphc027@gmail.com>
2021-02-22 16:55:16 +0800

    committing overloaded constructor to UserRegistrationv3-Branch

---
+++ src/com/gesz/controller/RegistrationController.java
@@ -0,0 +1,5 @@
+package com.gesz.controller;
+
+public class RegistrationController {
+
+}
--- src/com/gesz/model/User.java
+++ src/com/gesz/model/User.java
@@ -4,11 +4,30 @@ public class User {
 	private int id;
 	private String username;
 	private String password;
+	private String firstname;
+	private String lastname;
+	private String mi;
+	private String email;
+	private int contactno;
+	private String address;
 	private int isAdmin;
 	
 	public User(String username, String password) {
 		this.username = username;
 		this.password = password;
+	}
+	
+	public User(String username, String password,String firstname,String lastname,String mi,
+				String email,int contactno,String address,int isAdmin) {
+		this.username = username;
+		this.password = password;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.mi = mi;
+		this.email = email;
+		this.contactno = contactno;
+		this.address = address;
+		this.isAdmin = 0;
 	}
 
 	public int getId() {
