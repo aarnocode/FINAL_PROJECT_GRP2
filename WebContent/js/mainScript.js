6cd3a81fb2d84c925ab17e4b69afd9430edece6c
James Aaron Miranda <aarnocode@gmail.com>
2021-02-23 17:41:38 +0800

    Update mainScript.js
    update main script to redirect in landing page

--- WebContent/js/mainScript.js
+++ WebContent/js/mainScript.js
@@ -1,19 +1,12 @@
 /*IF JS USE <PAGE NAME><"SCRIPT">.js*/
 $(document).ready(function(){
-	/*$.ajax({
+	$.ajax({
 		url:contextPath+"home",
 		method:"POST",
 		success:function(result){
 			$("#mainContainer").html(result);
 		}
-	});*/
-	
-	$.ajax({
-		url:contextPath+"checkout",
-		method:"POST",
-		success:function(result){
-			window.location = "pages/checkout.jsp";
-		}
 	});
+	
 	
 });
