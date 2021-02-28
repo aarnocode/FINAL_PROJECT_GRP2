fbdf287e0b4844ece7c2c6fe5eed70c865204fa0
James Aaron Miranda <aarnocode@gmail.com>
2021-02-28 17:34:18 +0800

    Sticky navbar fixed
    Navbar fixed by removing height : 100% in body.
    
    Also redirect cart icon back to cart.jsp
    previously at ../
    my bad.

--- WebContent/js/header.js
+++ WebContent/js/header.js
@@ -1,109 +1,110 @@
 $(".loginBlur").hide();
-	$(".controlPanel").hide();
-	$("#btnLogout").hide();
-	$("#btnProfile").hide();
-	$("#btnOrder").hide();
-	$("#btnLogin").show();
-	var isLoggedIn = $("#isLoggedIn").val();
+$(".controlPanel").hide();
+$("#btnLogout").hide();
+$("#btnProfile").hide();
+$("#btnOrder").hide();
+$("#btnLogin").show();
+var isLoggedIn = $("#isLoggedIn").val();
 
-	$(document).ready(function(){
-		if(isLoggedIn == "true"){
-			$("#btnLogout").show();
-			$("#btnProfile").show();
-			$("#btnOrder").show();
-			$("#btnLogin").hide();
-		}
-	});
-	
-	window.onscroll = function() {myFunction()};
-	var header = document.getElementById("header");
-	var sticky = header.offsetTop;
-
-	function myFunction() {
-	  if (window.pageYOffset > sticky) {
-	    header.classList.add("sticky");
-	  } else {
-	    header.classList.remove("sticky");
-	  }
+$(document).ready(function(){
+	if(isLoggedIn == "true"){
+		$("#btnLogout").show();
+		$("#btnProfile").show();
+		$("#btnOrder").show();
+		$("#btnLogin").hide();
 	}
+});
 
-	$(".cart").click(function(){
-		console.log("cart clicked");
-		if(isLoggedIn == "true"){
-			$.ajax({
-				url: contextPath + "cart",
-				method: "POST",
-				success: function(){
-					window.location = "../";
-				}
-			});
-		}else{
-			$(".loginBlur").show();
-		}
-		
-	});
+window.onscroll = function() {myFunction()};
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
 
-	$("#btnNavHome").click(function(){
+function myFunction() {
+	console.log("sticky triggered");
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+$(".cart").click(function(){
+	console.log("cart clicked");
+	if(isLoggedIn == "true"){
 		$.ajax({
-			url: contextPath + "home",
+			url: contextPath + "cart",
 			method: "POST",
 			success: function(){
-				window.location = "../";
+				window.location = "../pages/cart.jsp";
 			}
 		});
-	});
-
-	$("#btnNavAccount").click(function(){
-		$(".controlPanel").slideToggle();
-	});
-
-	$("#btnProfile").click(function(){
-		if(isLoggedIn == "true"){
-			$.ajax({
-				url: contextPath + "pages/profile",
-				method: "POST",
-				success: function(){
-					window.location = "../pages/userProfile.jsp";
-				}
-			});
-		}else{
-			$(".loginBlur").show();
-		}
-	});
-	
-	$("#btnOrder").click(function(){
-		$.ajax({
-			url: contextPath + "pages/orderhistory",
-			method: "POST",
-			success: function(){
-				window.location = "../pages/userOrderHistory.jsp";
-			}
-		});
-	});
-
-	$("#btnLogout").click(function(){
-		$.ajax({
-			url: contextPath + "logout",
-			method:"POST",
-			success:function(){
-				window.location=contextPath;
-			}
-		});
-	});
-
-	$("#btnLogin").click(function(){
+	}else{
 		$(".loginBlur").show();
-	});
+	}
+	
+});
 
-	$("#btnClose").click(function(){
-		$(".txtUsername").val("");
-		$(".txtPassword").val("");
-		$("#notice").text("");
-		$(".loginBlur").hide();
-		if(isLoggedIn == "false"){
-			$.ajax({
-				url: contextPath + "resetstate",
-				method:"POST"
-			});
+$("#btnNavHome").click(function(){
+	$.ajax({
+		url: contextPath + "home",
+		method: "POST",
+		success: function(){
+			window.location = "../";
 		}
 	});
+});
+
+$("#btnNavAccount").click(function(){
+	$(".controlPanel").slideToggle();
+});
+
+$("#btnProfile").click(function(){
+	if(isLoggedIn == "true"){
+		$.ajax({
+			url: contextPath + "pages/profile",
+			method: "POST",
+			success: function(){
+				window.location = "../pages/userProfile.jsp";
+			}
+		});
+	}else{
+		$(".loginBlur").show();
+	}
+});
+
+$("#btnOrder").click(function(){
+	$.ajax({
+		url: contextPath + "pages/orderhistory",
+		method: "POST",
+		success: function(){
+			window.location = "../pages/userOrderHistory.jsp";
+		}
+	});
+});
+
+$("#btnLogout").click(function(){
+	$.ajax({
+		url: contextPath + "logout",
+		method:"POST",
+		success:function(){
+			window.location=contextPath;
+		}
+	});
+});
+
+$("#btnLogin").click(function(){
+	$(".loginBlur").show();
+});
+
+$("#btnClose").click(function(){
+	$(".txtUsername").val("");
+	$(".txtPassword").val("");
+	$("#notice").text("");
+	$(".loginBlur").hide();
+	if(isLoggedIn == "false"){
+		$.ajax({
+			url: contextPath + "resetstate",
+			method:"POST"
+		});
+	}
+});
